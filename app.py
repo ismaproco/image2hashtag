d45287768a035e2a485f4aebb93fb10d7f2de3f1
Ismapro <ismaproco@gmail.com>
2019-01-06 11:11:40 -0600

    Initial Commit

---
+++ app.py
@@ -0,0 +1,53 @@
+import os
+import base64
+from io import BytesIO
+
+from flask import Flask, make_response, request, render_template
+from flask_cors import CORS
+from werkzeug.exceptions import BadRequest
+import torch
+import evaluation
+
+"""
+floyd run --cpu --data floydhub/datasets/colornet/1:colornet --mode serve --env tensorflow-1.7
+"""
+app = Flask(__name__)
+CORS(app)
+app.config['DEBUG'] = False
+class_names = ['autumn', 'beach', 'christmas', 'easter', 'fashion', 'friends', 
+                   'happy', 'landscape', 'love', 'mountains', 'nature', 'snow', 'summer', 'sunset']
+device_to = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = evaluation.load_checkpoint(device_to, class_names)
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/image', methods=["POST"])
+def eval_image():
+    """"Preprocessing the data and evaluate the model"""
+    # check if the post request has the file part
+    input_file = request.files.get('file')
+    if not input_file:
+        return BadRequest("File not present in request")
+    if input_file.filename == '':
+        return BadRequest("File name is not present in request")
+    if not input_file.filename.lower().endswith(('.jpg', '.jpeg', '.png')):
+        return BadRequest("Invalid file type")
+
+    # # Save Image to process
+    input_buffer = BytesIO()
+    output_buffer = BytesIO()
+    input_file.save(input_buffer)
+    
+    
+    
+    img = evaluate_input(input_buffer)
+    response = evaluation.get_probs(input_buffer, model, device_to, class_names)
+    response.headers.set('Content-Type', 'application/json')
+    return response
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', threaded=False)
---
+++ url_files/generate_valid.sh
@@ -0,0 +1,12 @@
+head -10 beach.txt > beach_valid.txt && sed -i '1,+9d' beach.txt
+head -10 christmas.txt > christmas_valid.txt && sed -i '1,+9d' christmas.txt
+head -10 easter.txt > easter_valid.txt && sed -i '1,+9d' easter.txt
+head -10 fashion.txt > fashion_valid.txt && sed -i '1,+9d' fashion.txt
+head -10 friends.txt > friends_valid.txt && sed -i '1,+9d' friends.txt
+head -10 landscape.txt > landscape_valid.txt && sed -i '1,+9d' landscape.txt
+head -10 love.txt > love_valid.txt && sed -i '1,+9d' love.txt
+head -10 mountains.txt > mountains_valid.txt && sed -i '1,+9d' mountains.txt
+head -10 nature.txt > nature_valid.txt && sed -i '1,+9d' nature.txt
+head -10 snow.txt > snow_valid.txt && sed -i '1,+9d' snow.txt
+head -10 summer.txt > summer_valid.txt && sed -i '1,+9d' summer.txt
+head -10 sunset.txt > sunset_valid.txt && sed -i '1,+9d' sunset.txt
